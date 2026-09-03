caecf8c8dd811ba2bedb04b103c7683ce420dba5
= <=>
2026-09-03 11:12:13 -0300

    Algoritmoslide02

---
+++ src/AlgoritmoDoisAl.java
@@ -0,0 +1,15 @@
+public class AlgoritmoDoisAl {
+    
+    void main() {
+        IO.println("\nQuem ousa entrar na Sala do Eco?");
+        String nome = IO.readln();
+        IO.println("\nAh... " + nome + ". Eu esperava por você.");
+
+        IO.println("\nQuantos algoritmos você já venceu?");
+        String resposta = IO.readln();
+        // int total = resposta +1; isso não soma
+        
+        IO.println("\nQuantos algoritmos você já venceu?");
+
+    }
+}
